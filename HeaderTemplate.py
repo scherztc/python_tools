7180edd60d8803cd6310812aa50d3590b3d856ce
Thomas Scherz <scherztc@ucmail.uc.edu>
2023-11-02 23:37:43 -0400

    Adds physics tools.

---
+++ HeaderTemplate.py
@@ -0,0 +1,20 @@
+# Acticvity Python 1: Task 3
+# File: HW_10P2_TASK1_UCscherzts.py
+# Date:    2 Nov 2023
+# By:      Tre Scherz
+# Section: 014
+# Team:    236
+#
+# ELECTRONIC SIGNATURE
+#  Tre Scherz
+#
+# The electronic signature above indicatees the scripts 
+# submitted for evaluation is my individual work, and I
+# have a general understaning of all apsects of its
+# development and execution.
+#
+# A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+# This script is a header template that will be used for 
+# all your python files the rest of the semester.
+
+
